1d67442bb274419e5df6156523c5b4ede8be3223
Margarita Korzun <margarita.korzun@internetfusion.co.uk>
2021-09-29 20:54:35 +0300

    feat: implement count cats, carbon dating, dream team, and what season tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  return [].concat(...arr).filter((val) => val === "^^").length;
 }
 
 module.exports = {
