08c3eaa0d2204415f3942cc9dc7ef5afcdbb2571
Margarita Korzun <margarita.korzun@internetfusion.co.uk>
2022-03-01 16:12:25 +0300

    fix: resolve merge conflicts

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function deleteDigit(n) {
+//   const str = n.toString();
+//   let max = Number(str.substring(1, str.length));
+
+//   for (let i = 1; i <= str.length; i++) {
+//     let curr = Number(str.substring(0, i - 1) + str.substring(i));
+//     if (curr > max) { max = curr; }
+//   }
+
+//   return max;
+// }
+
+function deleteDigit(n) {
+  const str = n.toString();
+  let max = Number(str.substring(0, str.length - 1));
+
+  for (let i = 1; i < str.length; i++) {
+    let curr = Number(str.substring(0, i - 1) + str.substring(i));
+    if (curr > max) { max = curr; }
+  }
+
+  return max;
 }
 
 module.exports = {
